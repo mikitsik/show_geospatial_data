109f1cd3d9fccf94231e3a865d116a237dab52e1
Alex Mikitsik <mikitsik@gmail.com>
2023-10-09 16:14:13 +0300

    Add tests for tickets controller actions

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -3,12 +3,31 @@ require "rails_helper"
 RSpec.describe TicketsController do
   render_views
 
-  describe "GET #index" do
-    let!(:ticket) { Fabricate(:ticket) }
-    let!(:excavator) { Fabricate(:excavator, ticket:) }
+  let(:ticket1) { Fabricate(:ticket) }
+  let(:ticket2) { Fabricate(:ticket) }
 
-    it "d" do
-      debugger
+  before do
+    Fabricate(:excavator, ticket: ticket1)
+    Fabricate(:excavator, ticket: ticket2)
+  end
+
+  describe "GET #index" do
+    before { get :index, as: :html }
+
+    it { expect(assigns(:tickets)).to match_array([ticket1, ticket2]) }
+    it { assigns(:tickets).each { |ticket| expect(ticket.excavator).to be_present } }
+    it { expect(response).to render_template(:index) }
+    it { expect(response).to be_successful }
+  end
+
+  describe "GET #show" do
+    before { get :show, params: {id: ticket1.id} }
+
+    it { expect(assigns(:ticket)).to eq(ticket1) }
+    it { expect(response).to render_template(:show) }
+
+    context "when the ticket does not exist" do
+      it { expect { get :show, params: {id: 0} }.to raise_error(ActiveRecord::RecordNotFound) }
     end
   end
 end
--- spec/support/controller_helpers.rb
+++ spec/support/controller_helpers.rb
@@ -1,5 +1,2 @@
 module ControllerHelpers
-  def response_json
-    @response_json ||= JSON.parse(response.body)
-  end
 end
