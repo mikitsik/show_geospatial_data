c6ef5614f6d21dcd459b4e470ef8456d415f6bfc
Alex Mikitsik <mikitsik@gmail.com>
2023-10-16 01:15:20 +0300

    Fix route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   draw :api
   resources :tickets, only: [:show]
-  get "about/about", to: "about#about", as: "about"
+  get "about", to: "about#about", as: "about"
   root "tickets#index"
 end
